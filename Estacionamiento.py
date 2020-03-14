cd33d844ea50a33eec397d0ec8ed592b0dc2d68c
Saudyz <saudyzavala123@gmail.com>
2020-03-13 18:31:12 -0600

    mod

---
+++ Estacionamiento.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+
+class vehiculo:
+    
+    def __init__(self,Marca,Modelo,Tipo_de_vehículo,Hora_de_ingreso,Estado =""):
+        self.placa = placa
+        self.Marca = Marca
+        self.Modelo = Modelo
+        self.Tipo_de_vehículo = Tipo_de_vehículo
+        self.Hora de ingreso = Hora_de_ingreso
+        self.Estado = Estado
