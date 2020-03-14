5d1f5e0913ef9bb587531d084ddd65d1d5533348
Saudyz <saudyzavala123@gmail.com>
2020-03-13 18:30:37 -0600

    modificado

---
+++ Datos.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+
+class Datos:
+    """
+    En esta clase se tomaran los datos de los vehiculos 
+    que ingresen al estacionamiento
+
+    """
+
+    def __init__(self):
+        """ Inicializa los datos del sistema """
+        self.datos = list()
